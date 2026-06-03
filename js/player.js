b129c96c00f16031b7f63c7298b9773a0f4af00c
BraxtonW10 <161777065+BraxtonW10@users.noreply.github.com>
2026-06-03 03:26:26 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,22 @@
-// player file
+const player = {
+
+x: 8,
+y: 8,
+
+speed: 0.15,
+
+width: 32,
+height: 32,
+
+color: "#ff4444"
+
+};
+
+const keys = {
+
+up: false,
+down: false,
+left: false,
+right: false
+
+};
