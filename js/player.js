d23cc40c04ff16009a6c18efe9cd711407e1a93c
BraxtonW10 <161777065+BraxtonW10@users.noreply.github.com>
2026-06-03 04:51:04 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,23 +1,24 @@
 const player = {
 
-x: 8,
-y: 8,
+    x: 8,
+    y: 8,
 
-speed: 0.15,
+    speed: 0.15,
 
-width: 48,
-height: 48
+    width: 48,
+    height: 48
 
 };
 
 const playerSprite = new Image();
+
 playerSprite.src = "assets/player.png";
 
 const keys = {
 
-up: false,
-down: false,
-left: false,
-right: false
+    up: false,
+    down: false,
+    left: false,
+    right: false
 
 };
